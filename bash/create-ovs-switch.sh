a5d4f005e6ff42d6550fa3bb8c1220b663906cb3
Macauley Cheng <macauley_cheng@edge-core.com>
2021-11-12 11:58:29 +0800

    Update create-ovs-switch.sh

--- bash/create-ovs-switch.sh
+++ bash/create-ovs-switch.sh
@@ -9,10 +9,10 @@ for i in $(seq 1 $MAX_SWITCHES);
   do
         ovs-vsctl add-br br$i
         ovs-vsctl set bridge br$i protocols=OpenFlow13
-        ovs-vsctl set-controller br$i tcp:$CTRL_IP:6653
         datapath=$(printf "00000000000000%02x" $i)
         echo $datapath br$i
         ovs-vsctl set bridge br$i other-config:datapath-id=$datapath
+        ovs-vsctl set-controller br$i tcp:$CTRL_IP:6653
   done
 
 curl  -i -XPOST http://$CTRL_IP/mars/useraccount/v1/login --header 'Content-Type: application/json' --header 'Accept: application/json' -d  '{"user_name":"karaf", "password":"karaf"}' > session_temp
