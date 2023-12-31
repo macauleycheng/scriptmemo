afa00a82d692ef19da3fc6cc046b71ef097b4e45
Macauley Cheng <macauley_cheng@edge-core.com>
2023-12-31 11:43:02 +0800

    Create mars_29_locustfile.py

---
+++ locust/mars_29_locustfile.py
@@ -0,0 +1,25 @@
+from locust import HttpUser, task, between
+import hashlib
+
+class QuickstartUser(HttpUser):
+    session_header=""
+    wait_time = between(1, 2)
+    def on_start(self):
+        response = self.client.post("/mars/useraccount/v1/login", json={"user_name":"karaf", "password":hashlib.md5("Admin@admin123".encode("utf-8")).hexdigest()}, verify=False)
+        self.session_header = {"Cookie": "marsGSessionId=" + response.headers["mars_g_session_id"]}
+        #print("session_header", self.session_header)
+
+    @task
+    def getDevices(self):
+        response = self.client.get("/mars/v1/devices", headers=self.session_header)
+        #print(response.text)
+
+    @task
+    def getLinks(self):
+        response = self.client.get("/mars/v1/links", headers=self.session_header)
+        print(response.text)
+
+    @task
+    def getCluster(self):
+        response = self.client.get("/mars/v1/cluster", headers=self.session_header)
+        print(response.text)
